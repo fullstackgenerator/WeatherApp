a302ccaee95134d23b650518029dba21b90b0c54
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-18 12:49:20 +0200

    added dark mode and animations

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const toggle = document.getElementById("darkModeToggle");
+toggle.addEventListener("change", function () {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+});
+
+// Load preference on page load
+window.addEventListener("DOMContentLoaded", () => {
+    const isDark = localStorage.getItem("darkMode") === "true";
+    if (isDark) {
+        document.body.classList.add("dark-mode");
+        document.getElementById("darkModeToggle").checked = true;
+    }
+});
